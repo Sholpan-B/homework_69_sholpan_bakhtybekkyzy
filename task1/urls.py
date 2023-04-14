578e51f6dd422183b2524928ae9ba86e782e5118
Sholpan Bakhtybekkyzy <pasadena2929@gmail.com>
2023-04-14 16:35:50 +0600

    Добавлено приложение task1 по заданию 1

---
+++ task1/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+
+from task1.views import add, subtract, multiply, divide
+
+urlpatterns = [
+    path("add/", add, name='add'),
+    path("subtract/", subtract, name='subtract'),
+    path("multiply/", multiply, name='multiply'),
+    path("divide/", divide, name='divide'),
+]
